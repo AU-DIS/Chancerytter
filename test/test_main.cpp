2f45b1a550f0434fe8beaa56d89b6e5b8d8a2d19
Kasper Overgaard Mortensen <kasper@post2us.dk>
2023-10-24 14:44:37 +0200

    ☕️☕️ Formatting (#2)

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,14 +1,13 @@
 #include "gtest/gtest.h"
 #include "generaltests.h"
 
-
-
-TEST(TestOfTest, AssertTrue) {
+TEST(TestOfTest, AssertTrue)
+{
     ASSERT_TRUE(true);
 }
 
-
-int main(int argc, char **argv) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
